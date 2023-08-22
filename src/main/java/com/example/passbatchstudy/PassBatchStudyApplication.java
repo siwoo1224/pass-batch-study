c4d48753a1c631e380d77fa736850c5da476aff9
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-08-22 11:54:03 +0900

    init commit

---
+++ src/main/java/com/example/passbatchstudy/PassBatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.passbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassBatchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/passbatchstudy/PassBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
